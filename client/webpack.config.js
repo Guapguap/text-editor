cda7b53b4b414f50c7d75652bc0a8abc12c4daec
alberto londono <albertolondono.londono5@gmail.com>
2022-08-14 15:12:09 -0500

    Add InjectManifest for serviceworker file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,6 +23,11 @@ module.exports = () => {
         template: './index.html',
         title: 'JATE'
       }),
+       // service worker file
+       new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      })
     ],
 
     module: {
