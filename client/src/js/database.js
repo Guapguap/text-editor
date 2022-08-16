c8f5d980d262353dc07b7e7014d80396d3c95a5d
alberto londono <albertolondono.londono5@gmail.com>
2022-08-15 21:04:37 -0500

    Add getDb async function to complete transaction

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,6 +16,26 @@ const initdb = async () =>
 export const putDb = async (content) => console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+
+  // Create a connection to the database and version we want to use.
+  const jateDb = await openDB("jate", 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // use the .getAll() method to grab all the content in the DB
+  const request = store.getAll();
+
+  // Get confirmation of the request.
+  const result = await request;
+
+  // log result
+  console.log("Data saved to the jateDb", result);
+
+}
 
 initdb();
